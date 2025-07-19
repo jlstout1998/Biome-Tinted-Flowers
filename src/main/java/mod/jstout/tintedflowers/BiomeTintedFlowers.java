9516b3a434f4a2c34f9c23a878d42dacdb9f4e12
jlstout1998 <jlstout1998@gmail.com>
2025-07-18 21:24:27 -0700

    Update BiomeTintedFlowers.java
    Change tint from Foliage Color to Grass Color

--- src/main/java/mod/jstout/tintedflowers/BiomeTintedFlowers.java
+++ src/main/java/mod/jstout/tintedflowers/BiomeTintedFlowers.java
@@ -11,7 +11,7 @@ public class BiomeTintedFlowers implements ClientModInitializer {
   @Override
   public void onInitializeClient() {
     ColorProviderRegistry.BLOCK.register(
-      ((state, world, pos, tintIndex) -> world != null && pos != null ? BiomeColors.getAverageFoliageColor(world, pos) : GrassColor.getDefaultColor()),
+      ((state, world, pos, tintIndex) -> world != null && pos != null ? BiomeColors.getAverageGrassColor(world, pos) : GrassColor.getDefaultColor()),
       Blocks.ALLIUM, Blocks.AZURE_BLUET, Blocks.BLUE_ORCHID, Blocks.CORNFLOWER, Blocks.DANDELION, Blocks.LILY_OF_THE_VALLEY, Blocks.OXEYE_DAISY, Blocks.POPPY, Blocks.ORANGE_TULIP, Blocks.PINK_TULIP, Blocks.RED_TULIP, Blocks.WHITE_TULIP,
       Blocks.LILAC, Blocks.PEONY, Blocks.ROSE_BUSH, Blocks.SUNFLOWER, Blocks.TORCHFLOWER
     );
