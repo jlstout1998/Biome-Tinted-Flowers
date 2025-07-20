83bb370459cb2cc815962928c3cfc343197e73b6
jlstout1998 <jlstout1998@gmail.com>
2025-07-19 23:11:06 -0700

    Update BiomeTintedFlowers.java
    Add bushes

--- src/main/java/mod/jstout/tintedflowers/BiomeTintedFlowers.java
+++ src/main/java/mod/jstout/tintedflowers/BiomeTintedFlowers.java
@@ -14,7 +14,8 @@ public class BiomeTintedFlowers implements ClientModInitializer {
       ((state, world, pos, tintIndex) -> world != null && pos != null ? BiomeColors.getAverageGrassColor(world, pos) : GrassColor.getDefaultColor()),
       Blocks.ALLIUM, Blocks.AZURE_BLUET, Blocks.BLUE_ORCHID, Blocks.CORNFLOWER, Blocks.DANDELION, Blocks.LILY_OF_THE_VALLEY, Blocks.OXEYE_DAISY, Blocks.POPPY, Blocks.TORCHFLOWER_CROP, Blocks.TORCHFLOWER, Blocks.ORANGE_TULIP, Blocks.PINK_TULIP, Blocks.RED_TULIP, Blocks.WHITE_TULIP, Blocks.WITHER_ROSE,
       Blocks.LILAC, Blocks.PEONY, Blocks.PITCHER_CROP, Blocks.PITCHER_PLANT, Blocks.ROSE_BUSH, Blocks.SUNFLOWER,
-      Blocks.BEETROOTS, Blocks.CARROTS, Blocks.POTATOES, Blocks.WHEAT
+      Blocks.BEETROOTS, Blocks.CARROTS, Blocks.POTATOES, Blocks.WHEAT,
+      Blocks.FIREFLY_BUSH, Blocks.SWEET_BERRY_BUSH
     );
   }
 }
